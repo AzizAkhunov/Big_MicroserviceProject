e057d2c039a2027237cda716b3cec05f1a82739c
akhun <akhun@AZIZKHAN>
2023-12-05 23:50:08 +0500

    DbContext Added

---
+++ GAI.Infastructure/DbContexts/GAIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GAI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GAI.Infastructure.DbContexts
+{
+    public class GAIDbContext : DbContext
+    {
+        public GAIDbContext(DbContextOptions<GAIDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<YPX> GAies { get; set; }
+        public DbSet<Punishment> Punishments { get; set; }
+    }
+}
