39add133bb26422fa24bf88c68326cd442b1acfb
akhun <akhun@AZIZKHAN>
2023-12-06 19:44:48 +0500

    Product Model

--- OLX.Domain/Entities/Product.cs
+++ OLX.Domain/Entities/Product.cs
@@ -6,6 +6,11 @@ namespace OLX.Domain.Entities
     public class Product : BaseClassForModels
     {
         public string Name { get; set; }
-
+        public string Category { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+        public bool Status { get; set; } = false; //true bulsa sotuvda buladi false bulsa sotuvda bulmaydi
     }
 }
