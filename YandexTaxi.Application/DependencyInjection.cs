4aa23975ac63687334f63eb53ac263eab2c9a138
akhun <akhun@AZIZKHAN>
2023-12-07 07:45:21 +0500

    Dependency Injection

---
+++ YandexTaxi.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using YandexTaxi.Application.Interfaces;
+using YandexTaxi.Application.Services;
+
+namespace YandexTaxi.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            services.AddScoped<ICardService, CardService>();
+            services.AddScoped<ICarService, CarService>();
+            services.AddScoped<IClientService, ClientService>();
+            services.AddScoped<IDriverService, DriverService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IScrinService, ScrinService>();
+
+            return services;
+        }
+    }
+}
