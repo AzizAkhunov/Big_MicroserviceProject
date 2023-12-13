4fa3bfbe17ea2bc45c42d7054be4b833dd1a6daf
akhun <akhun@AZIZKHAN>
2023-12-13 17:20:51 +0500

    s

--- YandexTaxi.Api/Controllers/ScrinsController.cs
+++ YandexTaxi.Api/Controllers/ScrinsController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using YandexTaxi.Application.Interfaces;
 using YandexTaxi.Domain.DTOs;
